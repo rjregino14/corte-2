464c977af946611229a6d5c7ee7321ed008ce429
Ruber Regino <rjreginov@ul.edu.co>
2019-10-17 19:53:41 -0500

    agregados bilding e interpolacion

--- src/app/pages/biblioteca/autores/autores.component.ts
+++ src/app/pages/biblioteca/autores/autores.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autores.component.scss']
 })
 export class AutoresComponent implements OnInit {
+   
+  public titulo = "Formularios de Autores";
+  public nombre : String;
+  public apellido : String;
+  public fec_n :String;
+  public nacionalidad : String;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  saveForm() {
+    alert(this.nombre)
   }
 
 }
